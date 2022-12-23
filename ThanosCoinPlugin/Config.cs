01c5451ae02b3ef4cbb3c194ef18f9a64c816701
Adam Szerszenowicz <70147389+Pogromca-SCP@users.noreply.github.com>
2022-12-23 19:53:26 +0100

    Implemented basic plugin functions

---
+++ ThanosCoinPlugin/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace ThanosCoinPlugin
+{
+    public class Config
+    {
+        public bool CoinKillOnTails { get; set; } = true;
+
+        public string BalanceReason { get; set; } = "ThanosCoin.exe";
+    }
+}
---
+++ ThanosCoinPlugin/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using PluginAPI.Core;
+
+namespace ThanosCoinPlugin
+{
+    using EventManager = PluginAPI.Events.EventManager;
+
+    public class Plugin
+    {
+        [PluginConfig]
+        public Config PluginConfig;
+
+        [PluginPriority(LoadPriority.Medium)]
+        [PluginEntryPoint("Thanos Coin Plugin", "1.0.0", "Perfectly balanced plugin", "Adam Szerszenowicz")]
+        void LoadPlugin()
+        {
+            PrintLog("Plugin load started...");
+            EventManager.RegisterEvents(this);
+            PrintLog("Plugin is loaded.");
+        }
+
+        [PluginEvent(ServerEventType.PlayerCoinFlip)]
+        void OnPlayerCoinFlip(Player player, bool isTails)
+        {
+            if (player is null || player.IsGodModeEnabled || PluginConfig is null || PluginConfig.CoinKillOnTails != isTails)
+            {
+                return;
+            }
+
+            player.Kill(PluginConfig.BalanceReason ?? string.Empty);
+            PrintLog($"{player.DisplayNickname} got balanced."); 
+        }
+
+        private static void PrintLog(string message) => Log.Info(message, "ThanosCoinPlugin: ");
+    }
+}
