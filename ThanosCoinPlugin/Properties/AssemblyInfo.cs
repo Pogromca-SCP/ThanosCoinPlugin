4ed15ef5b015711a2bdd10b0252aa92dd369e2bb
Adam Szerszenowicz <70147389+Pogromca-SCP@users.noreply.github.com>
2023-03-30 22:03:10 +0200

    Copyright fix

--- ThanosCoinPlugin/Properties/AssemblyInfo.cs
+++ ThanosCoinPlugin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ThanosCoinPlugin")]
-[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyCopyright("Copyright © 2023 Adam Szerszenowicz")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
