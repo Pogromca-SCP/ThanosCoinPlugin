94f4b481b2814c2319fc7443880e9cf309b02695
Adam Szerszenowicz <70147389+Pogromca-SCP@users.noreply.github.com>
2023-06-14 16:51:13 +0200

    Updated assembly info

--- ThanosCoinPlugin/Properties/AssemblyInfo.cs
+++ ThanosCoinPlugin/Properties/AssemblyInfo.cs
@@ -3,8 +3,7 @@ using ThanosCoinPlugin;
 
 [assembly: AssemblyTitle("ThanosCoinPlugin")]
 [assembly: AssemblyDescription(Plugin.PluginDescription)]
-[assembly: AssemblyCompany(Plugin.PluginAuthor)]
 [assembly: AssemblyProduct("ThanosCoinPlugin")]
-[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyCopyright("Copyright © 2023 " + Plugin.PluginAuthor)]
 [assembly: AssemblyVersion(Plugin.PluginVersion)]
 [assembly: AssemblyFileVersion(Plugin.PluginVersion)]
